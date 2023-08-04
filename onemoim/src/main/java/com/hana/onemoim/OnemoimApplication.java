9de9ed443f20e47e042d8c688562a14bccd3d954
KORJaeyoungKim <jy0511_@naver.com>
2023-08-04 15:39:54 +0900

    포로젝트 생성

---
+++ onemoim/src/main/java/com/hana/onemoim/OnemoimApplication.java
@@ -0,0 +1,13 @@
+package com.hana.onemoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemoimApplication.class, args);
+	}
+
+}
---
+++ onemoim/src/test/java/com/hana/onemoim/OnemoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.onemoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnemoimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
